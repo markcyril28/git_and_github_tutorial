230cc39bfdc4a916309140b8559fa7ef6e3dee84
Mark Cyril <mercadomarkcyril28@gmail.com>
2022-02-25 21:03:32 +0800

    Event added: Into the new world.

--- events.py
+++ events.py
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
 events = ["First Successful Astral Head Transplant", "Cooked a meal", "Successful suob session", "Joined the Kamar Taj", "Joined the Kamar Taj", "Become the sorcerer defender"]
 events.append("Defender Strange meet Chavez America")
 for event in events:
     print(event)
+
+events.append("Into the new world")
