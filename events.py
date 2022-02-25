a380dc82f263b1e98ebf78b4d0920436d47c3066
Mark Cyril <mercadomarkcyril28@gmail.com>
2022-02-25 18:45:43 +0800

    hello

---
+++ events.py
@@ -0,0 +1,4 @@
+events = ["First Successful Astral Head Transplant", "Cooked a meal", "Successful suob session"]
+
+for event in events:
+    print(event)
---
+++ spells.py
@@ -0,0 +1,4 @@
+spells = ["Astral Surgery", "Strings Scissor", "Time Manipulation"]
+
+for spell in spells:
+    print(spell)
