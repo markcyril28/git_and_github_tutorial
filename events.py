bce77c00ee0c6dcd41097cee1af7ae079ddfe597
Mark Cyril <mercadomarkcyril28@gmail.com>
2022-02-25 19:01:18 +0800

    Defender Strange is learning some spells and things is progressing for his timeline.

---
+++ events.py
@@ -0,0 +1,4 @@
+events = ["Joined the Kamar Taj", "Become the sorcerer defender"]
+
+for event in events:
+    print(event)
---
+++ spells.py
@@ -0,0 +1,4 @@
+spells = ["Summoning the ring of seven rings of raggadorr", "Light of agamotto"]
+
+for spell in spells:
+    print(spell)
